604ad0d7ee845119e44dada5898ff598b8fbb68d
Christopher Buckley <15166572+topherbuckley@users.noreply.github.com>
2022-12-01 11:48:59 +0900

    Gerbers sent to PCBWAY

--- kicad/robot/bom.py
+++ kicad/robot/bom.py
@@ -1,71 +1,32 @@
-"""
-    @package
-    Output: CSV (comma-separated)
-    Grouped By: Value, Footprint
-    Sorted By: Ref
-    Fields: Ref, Qnty, Value, Cmp name, Footprint, Description, Vendor
-
-    Command line:
-    python3 [myproject.xml] [output.csv] [number of boards]"
-"""
-
 # Import the KiCad python helper module and the csv formatter
 import kicad_netlist_reader
 import csv
 import sys
+from pathlib import Path
 
 # Generate an instance of a generic netlist, and load the netlist tree from
 # the command line option. If the file doesn't exist, execution will stop
-net = kicad_netlist_reader.netlist(sys.argv[1])
+net = kicad_netlist_reader.netlist("robot.xml")
 
-# Open a file to write to, if the file cannot be opened output to stdout
-# instead
+bomFile = Path('robot_bom.csv')
+toBuy = Path('toBuy.csv')
 
-# look for PCB-NNN.yml file in output directory and then name output file to
-# PCB-NNN.csv. If PCB-NNN.csv file exists, then replace it.
-
-from pathlib import Path
-
-bomFile = Path(sys.argv[2])
-bomDir = bomFile.parent
-
-ymlFiles = sorted(Path(bomDir).glob('PCB-*.yml'))
-if len(ymlFiles) == 1:
-    print("Found yml file: ", ymlFiles[0].name)
-    bomFile = bomDir / Path(ymlFiles[0].stem + ".csv")
-else:
-    # look for existing BOM and overwite it
-    pcbCsvFiles = sorted(Path(bomDir).glob('PCB-*.csv'))
-    if len(pcbCsvFiles) == 1:
-        print("Found existing bom file: ", pcbCsvFiles[0].name)
-        bomFile = pcbCsvFiles[0]
-    else:
-        print()
-        print("WARNING: Did not find yml config file, please create a "
-              + "PCB-NNN.yml config file so we "
-              + "know the part number for this PCB.")
-        print()
-
-print("Outputing bomfile: ", str(bomFile))
+batch = 5
 
 try:
-    f = open(str(bomFile), 'w')
+    bomFile = open(str(bomFile), 'w')
+    toBuy = open(str(toBuy), 'w')
 except IOError:
-    e = "Can't open output file for writing: " + sys.argv[2]
+    e = "Can't open output file for writing: "
     print(__file__, ":", e, sys.stderr)
     f = sys.stdout
 
 # Create a new csv writer object to use as the output formatter
-out = csv.writer(f, lineterminator='\n', delimiter=';', quotechar='\"', quoting=csv.QUOTE_ALL)
+out_bomFile = csv.writer(bomFile, lineterminator='\n', delimiter=';', quotechar='\"', quoting=csv.QUOTE_ALL)
+out_toBuy = csv.writer(toBuy, lineterminator='\n', delimiter=';', quotechar='\"', quoting=csv.QUOTE_ALL)
 
-# Output a set of rows for a header providing general information
-# don't write header info as that breaks automatic parsing by CSV tools
-#out.writerow(['Source:', net.getSource()])
-#out.writerow(['Date:', net.getDate()])
-#out.writerow(['Tool:', net.getTool()])
-#out.writerow( ['Generator:', sys.argv[0]] )
-#out.writerow(['Component Count:', len(net.components)])
-out.writerow(['Ref', 'Qnty', 'Value', 'Manufacturer', 'Part Number', 'Description'])
+out_bomFile.writerow(['Ref', 'Qty', 'Value', 'Manufacturer', 'Part Number', 'Description', 'Footprint', 'Type'])
+out_toBuy.writerow(['Ref', 'Qty', 'Value', 'Manufacturer', 'Part Number', 'Description', 'Footprint', 'Type'])
 
 # Get all of the components in groups of matching parts + values
 # (see ky_generic_netlist_reader.py)
@@ -81,28 +42,22 @@ for group in grouped:
         refs += component.getRef() + ","
         c = component
         quantity = len(group)
-        if len(sys.argv) > 3:
-            if sys.argv[3].isnumeric():
-                totalQty = quantity * int(sys.argv[3])
-        else:
-            totalQty = quantity
+        totalQty = quantity * batch
         try:
             stock = int(c.getField("Stock"))
         except:
             stock = 0
 
-
-
     refs = refs.removesuffix(",")
 
-    if len(sys.argv) <= 3: # Number of boards not given
-        if c.getValue() != "DNP" and c.getField("Part Number") != "N/A" and (totalQty - stock) > 0:
-            out.writerow([refs, quantity, c.getValue(), c.getField("Manufacturer"), c.getField("Part Number"),
-                          c.getDescription()])
+    # Normal BOM
+    if c.getValue() != "DNP" and c.getField("Part Number") != "N/A":
+        out_bomFile.writerow([refs, quantity, c.getValue(), c.getField("Manufacturer"), c.getField("Part Number"),
+                      c.getDescription(), c.getFootprint()])
 
-    else:
-        # Fill in the component groups common data
-        if c.getValue() != "DNP" and c.getField("Part Number") != "N/A" and (totalQty - stock) > 0:
-            out.writerow([refs, quantity, c.getValue(), c.getField("Manufacturer"), c.getField("Part Number"),
-                          c.getDescription()])
+    # To Buy BOM
+    # Fill in the component groups common data
+    if c.getValue() != "DNP" and c.getField("Part Number") != "N/A" and (totalQty - stock) > 0:
+        out_toBuy.writerow([refs, quantity, c.getValue(), c.getField("Manufacturer"), c.getField("Part Number"),
+                      c.getDescription(), c.getFootprint()])
 
